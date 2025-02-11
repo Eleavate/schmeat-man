80bce6dd7c99c57eb65841777a24589255a792da
Hargaflarb <emmabrocher@gmail.com>
2025-02-11 10:19:58 +0100

    added basic classes

---
+++ Schmeat-Game/Schmeat-Game/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schmeat_Game
+{
+    public class Employee:GameObject
+{
+}
+}
---
+++ Schmeat-Game/Schmeat-Game/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schmeat_Game
+{
+    public class GameObject
+    {
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
