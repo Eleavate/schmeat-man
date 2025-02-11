fcde85b3b99f5e288c04dd15f4f3ba9ab7fa93e9
Hargaflarb <emmabrocher@gmail.com>
2025-02-11 11:12:08 +0100

    added some more stuff, employee spawns but is not functional

--- Schmeat-Game/Schmeat-Game/Employee.cs
+++ Schmeat-Game/Schmeat-Game/Employee.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
 
 namespace Schmeat_Game
 {
-    public class Employee:GameObject
-{
-}
+    public class Employee : GameObject
+    {
+        Thread employeeThread = new Thread(ActiveThread);
+        public enum Carrying { Nothing,Meat,PreparedMeat};
+
+        Carrying currentlyCarrying = Carrying.Nothing;
+
+        public Employee() 
+        {
+            employeeThread.IsBackground = true;
+            employeeThread.Start();
+            scale=0.05f;
+            position = new Vector2(200, 400);
+        }
+        public override void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>("temp sprite");
+        }
+
+        static void ActiveThread()
+        {
+            //get job 
+            //do job (call)
+        }
+
+    }
 }
--- Schmeat-Game/Schmeat-Game/GameObject.cs
+++ Schmeat-Game/Schmeat-Game/GameObject.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct2D1.Effects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,22 @@ namespace Schmeat_Game
 {
     public class GameObject
     {
-        public void LoadContent(ContentManager content)
+        protected Texture2D sprite;
+        protected float scale = 1;
+        protected float layer = 0;
+        protected Vector2 position;
+        public virtual void LoadContent(ContentManager content)
         {
-
+            
         }
 
         public void Update(GameTime gameTime)
         {
-
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(sprite, position, null, Color.White, 0, new Vector2(sprite.Width / 2, sprite.Height / 2), scale, SpriteEffects.None, layer);
         }
     }
 }
