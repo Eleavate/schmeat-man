be9bce7d8ca27aac9c92a2345d71c20f8daa69b5
Eleavate <echoovervad@gmail.com>
2025-02-13 09:55:39 +0100

    Cutting board added

--- Schmeat-Game/Schmeat-Game/CuttingBoard.cs
+++ Schmeat-Game/Schmeat-Game/CuttingBoard.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Schmeat_Game
 {
-    internal class CuttingBoard
+    public class CuttingBoard : Workspace
     {
+        //Fields
 
+        //Properties
+
+        //Constructor
+        public CuttingBoard(Vector2 position)
+        {
+            this.position = position;
+        }
+        //Methods
+        public static void Prepare()
+        {
+            Thread.Sleep(500);
+            
+        }
     }
 }
